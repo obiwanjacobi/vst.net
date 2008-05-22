2869315267a1eccfaafcd95b3aca74677486f949
obiwanjacobi_cp <SND\obiwanjacobi_cp>
2008-05-22 05:13:18 +0000

    added error handling in Interop and some project file changes for release build.

--- Source/Code/Jacobi.Vst.Framework.TestPlugin/Properties/AssemblyInfo.cs
+++ Source/Code/Jacobi.Vst.Framework.TestPlugin/Properties/AssemblyInfo.cs
@@ -12,16 +12,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("68ebd384-592d-4d65-88f0-ea0ad6de93a6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
--- Source/Code/Jacobi.Vst.Interop/AssemblyInfo.cpp
+++ Source/Code/Jacobi.Vst.Interop/AssemblyInfo.cpp
@@ -9,23 +9,9 @@ using namespace System::Runtime::CompilerServices;
 // associated with an assembly.
 //
 [assembly:AssemblyTitleAttribute("Jacobi.Vst.Interop")];
-[assembly:AssemblyDescriptionAttribute("C++ <> .NET Interop layer.")];
+[assembly:AssemblyDescriptionAttribute("C++ <=> .NET Interop layer.")];
 [assembly:AssemblyProductAttribute("VST.NET")];
 [assembly:AssemblyCultureAttribute("")];
-
-//
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the value or you can default the Revision and Build Numbers
-// by using the '*' as shown below:
-
-[assembly:AssemblyVersionAttribute("0.1.0.0")];
-[assembly:AssemblyFileVersionAttribute("0.1.0.0")];
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
