ee27d90cabe79e5f9529aa0175b579e0d227a333
Marc Jacobi <obiwanjacobi@hotmail.com>
2008-12-20 17:57:20 +0000

    added the beginings of a host sample.

---
+++ Source/Samples/Jacobi.Vst.Samples.Host/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jacobi.Vst.Samples.Host")]
+[assembly: AssemblyDescription("A very simplistic Host that demonstrates the usage of the host interop objects.")]
+[assembly: AssemblyProduct("VST.NET Sample")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e5438cbf-4822-48fe-b744-136676e62b89")]
