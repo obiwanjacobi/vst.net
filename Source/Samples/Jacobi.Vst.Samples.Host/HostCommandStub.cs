76cdaf92f1f7e9815e20da0ccdab124cef8e9d86
Marc Jacobi <obiwanjacobi@hotmail.com>
2008-12-20 17:57:20 +0000

    added the beginings of a host sample.

---
+++ Source/Samples/Jacobi.Vst.Samples.Host/HostCommandStub.cs
@@ -0,0 +1,195 @@
+﻿using System;
+
+using Jacobi.Vst.Core.Host;
+using Jacobi.Vst.Interop.Host;
+
+namespace Jacobi.Vst.Samples.Host
+{
+    class PluginCalledEventArgs : EventArgs
+    {
+        public PluginCalledEventArgs(string message)
+        {
+            Message = message;
+        }
+
+        public string Message { get; private set; }
+    }
+
+    class HostCommandStub : IVstHostCommandStub 
+    {
+        public event EventHandler<PluginCalledEventArgs> PluginCalled;
+
+        private void RaisePluginCalled(string message)
+        {
+            EventHandler<PluginCalledEventArgs> handler = PluginCalled;
+
+            if(handler != null)
+            {
+                handler(this, new PluginCalledEventArgs(message));
+            }
+        }
+
+        #region IVstHostCommandsStub Members
+
+        public IVstPluginContext PluginContext { get; set; }
+        
+        #endregion
+
+        #region IVstHostCommands20 Members
+
+        public bool BeginEdit(int index)
+        {
+            RaisePluginCalled("BeginEdit(" + index + ")");
+
+            return false;
+        }
+
+        public Jacobi.Vst.Core.VstCanDoResult CanDo(Jacobi.Vst.Core.VstHostCanDo cando)
+        {
+            RaisePluginCalled("CanDo(" + cando + ")");
+            return Jacobi.Vst.Core.VstCanDoResult.Unknown;
+        }
+
+        public bool CloseFileSelector(Jacobi.Vst.Core.VstFileSelect fileSelect)
+        {
+            RaisePluginCalled("CloseFileSelector(" + fileSelect.Command + ")");
+            return false;
+        }
+
+        public bool EndEdit(int index)
+        {
+            RaisePluginCalled("EndEdit(" + index + ")");
+            return false;
+        }
+
+        public Jacobi.Vst.Core.VstAutomationStates GetAutomationState()
+        {
+            RaisePluginCalled("GetAutomationState()");
+            return Jacobi.Vst.Core.VstAutomationStates.Off;
+        }
+
+        public int GetBlockSize()
+        {
+            RaisePluginCalled("GetBlockSize()");
+            return 0;
+        }
+
+        public string GetDirectory()
+        {
+            RaisePluginCalled("GetDirectory()");
+            return null;
+        }
+
+        public int GetInputLatency()
+        {
+            RaisePluginCalled("GetInputLatency()");
+            return 0;
+        }
+
+        public Jacobi.Vst.Core.VstHostLanguage GetLanguage()
+        {
+            RaisePluginCalled("GetLanguage()");
+            return Jacobi.Vst.Core.VstHostLanguage.NotSupported;
+        }
+
+        public int GetOutputLatency()
+        {
+            RaisePluginCalled("GetOutputLatency()");
+            return 0;
+        }
+
+        public Jacobi.Vst.Core.VstProcessLevels GetProcessLevel()
+        {
+            RaisePluginCalled("GetProcessLevel()");
+            return Jacobi.Vst.Core.VstProcessLevels.Unknown;
+        }
+
+        public string GetProductString()
+        {
+            RaisePluginCalled("GetProductString()");
+            return "VST.NET";
+        }
+
+        public float GetSampleRate()
+        {
+            RaisePluginCalled("GetSampleRate()");
+            return 0f;
+        }
+
+        public Jacobi.Vst.Core.VstTimeInfo GetTimeInfo(Jacobi.Vst.Core.VstTimeInfoFlags filterFlags)
+        {
+            RaisePluginCalled("GetTimeInfo(" + filterFlags + ")");
+            return null;
+        }
+
+        public string GetVendorString()
+        {
+            RaisePluginCalled("GetVendorString()");
+            return "Jacobi Software";
+        }
+
+        public int GetVendorVersion()
+        {
+            RaisePluginCalled("GetVendorVersion()");
+            return 1000;
+        }
+
+        public bool IoChanged()
+        {
+            RaisePluginCalled("IoChanged()");
+            return false;
+        }
+
+        public bool OpenFileSelector(Jacobi.Vst.Core.VstFileSelect fileSelect)
+        {
+            RaisePluginCalled("OpenFileSelector(" + fileSelect.Command + ")");
+            return false;
+        }
+
+        public bool ProcessEvents(Jacobi.Vst.Core.VstEvent[] events)
+        {
+            RaisePluginCalled("ProcessEvents(" + events.Length + ")");
+            return false;
+        }
+
+        public bool SizeWindow(int width, int height)
+        {
+            RaisePluginCalled("SizeWindow(" + width + ", " + height + ")");
+            return false;
+        }
+
+        public bool UpdateDisplay()
+        {
+            RaisePluginCalled("UpdateDisplay()");
+            return false;
+        }
+
+        #endregion
+
+        #region IVstHostCommands10 Members
+
+        public int GetCurrentPluginID()
+        {
+            RaisePluginCalled("GetCurrentPluginID()");
+            return PluginContext.PluginInfo.PluginID;
+        }
+
+        public int GetVersion()
+        {
+            RaisePluginCalled("GetVersion()");
+            return 1000;
+        }
+
+        public void ProcessIdle()
+        {
+            RaisePluginCalled("ProcessIdle()");
+        }
+
+        public void SetParameterAutomated(int index, float value)
+        {
+            RaisePluginCalled("SetParameterAutomated(" + index + ", " + value + ")");
+        }
+
+        #endregion
+    }
+}
---
+++ Source/Samples/Jacobi.Vst.Samples.Host/MainForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+using Jacobi.Vst.Core;
+using Jacobi.Vst.Interop.Host;
+using System.Diagnostics;
+
+namespace Jacobi.Vst.Samples.Host
+{
+    public partial class MainForm : Form
+    {
+        private List<VstPluginContext> _plugins = new List<VstPluginContext>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void FillPluginList()
+        {
+            PluginListVw.Items.Clear();
+
+            foreach (VstPluginContext ctx in _plugins)
+            {
+                ListViewItem lvItem = new ListViewItem(ctx.PluginCommandStub.GetEffectName());
+                lvItem.SubItems.Add(ctx.PluginCommandStub.GetProductString());
+                lvItem.SubItems.Add(ctx.PluginCommandStub.GetVendorString());
+                lvItem.SubItems.Add(ctx.PluginCommandStub.GetVendorVersion().ToString());
+                lvItem.SubItems.Add(ctx.Find<string>("PluginPath"));
+                lvItem.Tag = ctx;
+
+                PluginListVw.Items.Add(lvItem);
+            }
+        }
+
+        private VstPluginContext OpenPlugin(string pluginPath)
+        {
+            HostCommandStub hostCmdStub = new HostCommandStub();
+            VstPluginContext ctx = new VstPluginContext(hostCmdStub);
+
+            ctx.Set("PluginPath", pluginPath);
+            ctx.Set("HostCmdStub", hostCmdStub);
+
+            hostCmdStub.PluginCalled += new EventHandler<PluginCalledEventArgs>(HostCmdStub_PluginCalled);
+
+            ctx.Initialize(pluginPath);
+
+            return ctx;
+        }
+
+        private void ReleaseAllPlugins()
+        {
+            foreach (VstPluginContext ctx in _plugins)
+            {
+                ctx.Dispose();
+            }
+
+            _plugins.Clear();
+        }
+
+        private void HostCmdStub_PluginCalled(object sender, PluginCalledEventArgs e)
+        {
+            HostCommandStub hostCmdStub = (HostCommandStub)sender;
+
+            Debug.WriteLine("Plugin " + hostCmdStub.PluginContext.PluginInfo.PluginID + " called" + e.Message);
+        }
+
+        private void BrowseBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDlg.FileName = PluginPathTxt.Text;
+
+            if (OpenFileDlg.ShowDialog(this) == DialogResult.OK)
+            {
+                PluginPathTxt.Text = OpenFileDlg.FileName;
+            }
+        }
+
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            VstPluginContext ctx = OpenPlugin(PluginPathTxt.Text);
+
+            _plugins.Add(ctx);
+
+            FillPluginList();
+        }
+
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            ReleaseAllPlugins();
+        }
+    }
+}
