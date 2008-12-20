e5484a18c48cc89cdac69a57289a3b3f7e4ee17b
Marc Jacobi <obiwanjacobi@hotmail.com>
2008-12-20 08:41:05 +0000

    enhanced the VstPluginContext with a generic dictionary and declared an interface to be shared to the command stubs.

--- Source/Code/Jacobi.Vst.Interop/Host/VstPluginContext.h
+++ Source/Code/Jacobi.Vst.Interop/Host/VstPluginContext.h
@@ -7,22 +7,40 @@ namespace Vst {
 namespace Interop {
 namespace Host
 {
-
-	public ref class VstPluginContext : public System::IDisposable
+	/// <summary>
+	/// 
+	/// </summary>
+	public ref class VstPluginContext : public Jacobi::Vst::Core::Host::IVstPluginContext, public System::IDisposable
 	{
 	public:
 		VstPluginContext(Jacobi::Vst::Core::Host::IVstHostCommandStub^ hostCmdStub);
+		// IDisposable interface implementation
 		~VstPluginContext();
 		!VstPluginContext();
 
 		void Initialize(System::String^ pluginPath);
-		void AcceptPluginInfoData(System::Boolean raiseEvents);
+		
+		// IVstPluginContext interface implementation
+		generic<typename T> 
+		virtual void Set(System::String^ keyName, T value);
+		generic<typename T> 
+		virtual T Find(System::String^ keyName);
+		virtual void Remove(System::String^ keyName)
+		{ _props->Remove(keyName); }
 
-		property Jacobi::Vst::Core::Host::IVstPluginCommandStub^ PluginCommandStub 
+		virtual property Jacobi::Vst::Core::Host::IVstHostCommandStub^ HostCommandStub 
+		{ Jacobi::Vst::Core::Host::IVstHostCommandStub^ get() { return _hostCmdStub; } }
+
+		virtual property Jacobi::Vst::Core::Host::IVstPluginCommandStub^ PluginCommandStub 
 		{ Jacobi::Vst::Core::Host::IVstPluginCommandStub^ get() { return _pluginCmdStub; } }
 
-		property Jacobi::Vst::Core::Plugin::VstPluginInfo^ PluginInfo 
+		virtual property Jacobi::Vst::Core::Plugin::VstPluginInfo^ PluginInfo 
 		{ Jacobi::Vst::Core::Plugin::VstPluginInfo^ get() { return _pluginInfo; } }
+
+		virtual void AcceptPluginInfoData(System::Boolean raiseEvents);
+
+		// INotifyPropertyChanged interface implementation
+		virtual event System::ComponentModel::PropertyChangedEventHandler^ PropertyChanged;
 
 	internal:
 		// only set during loading of plugin
@@ -35,11 +53,18 @@ namespace Host
 		HMODULE _hLib;
 		::AEffect* _pEffect;
 
+		Jacobi::Vst::Core::Host::IVstHostCommandStub^ _hostCmdStub;
 		VstHostCommandProxy^ _hostCmdProxy;
 		VstPluginCommandStub^ _pluginCmdStub;
 		Jacobi::Vst::Core::Plugin::VstPluginInfo^ _pluginInfo;
 
-		void CloseLibrary() { if(_hLib != NULL) { ::FreeLibrary(_hLib); _hLib = NULL; } }
+		System::Collections::Generic::Dictionary<System::String^, System::Object^>^ _props;
+
+		void CloseLibrary()
+		{ if(_hLib != NULL) { ::FreeLibrary(_hLib); _hLib = NULL; } }
+
+		void RaisePropertyChanged(System::String^ propName)
+		{ PropertyChanged(this, gcnew	System::ComponentModel::PropertyChangedEventArgs(propName));}
 	};
 
 }}}} // namespace Jacobi.Vst.Interop.Host
