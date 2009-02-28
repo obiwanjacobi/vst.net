4aaf9ef00ab6ce1b69c4578f4aa5d2847cbafad3
Marc Jacobi <obiwanjacobi@hotmail.com>
2009-02-28 13:59:30 +0000

    Added more base classes for common interfaces. VstPluginAudio(Precision)ProcessorBase, VstPluginProgramsBase, VstPluginPresistenceBase together with VstProgramWriter and VstProgramReaderBase (IO).

---
+++ Source/Code/Jacobi.Vst.Framework/Plugin/IO/VstProgramReaderBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Jacobi.Vst.Framework.Plugin.IO
+{
+    public abstract class VstProgramReaderBase
+    {
+        public VstProgramReaderBase(Stream input)
+        {
+            Reader = new BinaryReader(input);
+        }
+
+        public VstProgramReaderBase(Stream input, Encoding encoding)
+        {
+            Reader = new BinaryReader(input, encoding);
+        }
+
+        protected BinaryReader Reader { get; private set; }
+
+        protected abstract VstProgram CreateProgram();
+
+        public void ReadPrograms(VstProgramCollection programs)
+        {
+            int count = ReadProgramHeader(programs);
+
+            for (int i = 0; i < count; i++)
+            {
+                VstProgram program = ReadProgram();
+                
+                programs.Add(program);
+            }
+        }
+
+        public virtual VstProgram ReadProgram()
+        {
+            VstProgram program = CreateProgram();
+
+            program.Name = Reader.ReadString();
+
+            ReadParameters(program.Parameters);
+
+            return program;
+        }
+
+        public void ReadParameters(VstParameterCollection parameters)
+        {
+            int count = ReadParameterHeader(parameters);
+
+            for (int i = 0; i < count; i++)
+            {
+                string name = Reader.ReadString();
+                float value = Reader.ReadSingle();
+
+                if (parameters.Contains(name))
+                {
+                    VstParameter parameter = parameters[name];
+                    parameter.Value = value;
+                }
+            }
+        }
+
+        protected virtual int ReadProgramHeader(VstProgramCollection programs)
+        {
+            string typeName = Reader.ReadString();
+            int count = Reader.ReadInt32();
+
+            return count;
+        }
+
+        protected virtual int ReadParameterHeader(VstParameterCollection parameters)
+        {
+            string typeName = Reader.ReadString();
+            int count = Reader.ReadInt32();
+
+            return count;
+        }
+    }
+}
---
+++ Source/Code/Jacobi.Vst.Framework/Plugin/IO/VstProgramWriter.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Jacobi.Vst.Framework.Plugin.IO
+{
+    /// <summary>
+    /// This class writes parameter values of programs to a stream.
+    /// </summary>
+    /// <remarks>The objects themselves are not serialized, only identifying information (name) and the parameter values.</remarks>
+    public class VstProgramWriter
+    {
+        public VstProgramWriter(Stream output)
+        {
+            Writer = new BinaryWriter(output);
+        }
+
+        public VstProgramWriter(Stream output, Encoding encoding)
+        {
+            Writer = new BinaryWriter(output, encoding);
+        }
+
+        protected BinaryWriter Writer { get; private set; }
+
+        public void Write(VstProgramCollection programs)
+        {
+            WriteProgramHeader(programs);
+
+            foreach (VstProgram program in programs)
+            {
+                Write(program);
+            }
+        }
+
+        public virtual void Write(VstProgram program)
+        {
+            Writer.Write(program.Name);
+            Write(program.Parameters);
+        }
+
+        public void Write(VstParameterCollection parameters)
+        {
+            WriteParameterHeader(parameters);
+
+            foreach (VstParameter parameter in parameters)
+            {
+                Write(parameter);
+            }
+        }
+
+        public virtual void Write(VstParameter parameter)
+        {
+            Writer.Write(parameter.Info.Name);
+            Writer.Write(parameter.Value);
+        }
+
+        protected virtual void WriteProgramHeader(VstProgramCollection programs)
+        {
+            Writer.Write(typeof(VstProgram).Name);
+            Writer.Write(programs.Count);
+        }
+
+        protected virtual void WriteParameterHeader(VstParameterCollection parameters)
+        {
+            Writer.Write(typeof(VstParameter).Name);
+            Writer.Write(parameters.Count);
+        }
+    }
+}
