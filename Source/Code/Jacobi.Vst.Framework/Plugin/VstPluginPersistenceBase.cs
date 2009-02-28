64655253b2a108a929dbd6bcd3d5bb25b727e136
Marc Jacobi <obiwanjacobi@hotmail.com>
2009-02-28 13:59:30 +0000

    Added more base classes for common interfaces. VstPluginAudio(Precision)ProcessorBase, VstPluginProgramsBase, VstPluginPresistenceBase together with VstProgramWriter and VstProgramReaderBase (IO).

---
+++ Source/Code/Jacobi.Vst.Framework/Plugin/VstPluginPersistenceBase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using Jacobi.Vst.Core;
+using Jacobi.Vst.Framework.Plugin.IO;
+
+namespace Jacobi.Vst.Framework.Plugin
+{
+    public abstract class VstPluginPersistenceBase : IVstPluginPersistence
+    {
+        public VstPluginPersistenceBase()
+        {
+            Encoding = Encoding.ASCII;
+        }
+
+        public Encoding Encoding { get; set; }
+
+        #region IVstPluginPersistence Members
+
+        public virtual void ReadPrograms(Stream stream, VstProgramCollection programs)
+        {
+            VstProgramReaderBase reader = CreateProgramReader(stream);
+
+            reader.ReadPrograms(programs);
+        }
+
+        public virtual void WritePrograms(Stream stream, VstProgramCollection programs)
+        {
+            VstProgramWriter writer = new VstProgramWriter(stream, Encoding);
+
+            writer.Write(programs);
+        }
+
+        public virtual bool CanLoadChunk(VstPatchChunkInfo chunkInfo)
+        {
+            return true;
+        }
+
+        #endregion
+
+        protected abstract VstProgramReaderBase CreateProgramReader(Stream input);
+    }
+}
---
+++ Source/Code/Jacobi.Vst.Framework/Plugin/VstPluginProgramsBase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Jacobi.Vst.Framework.Plugin
+{
+    public abstract class VstPluginProgramsBase : IVstPluginPrograms
+    {
+        #region IVstPluginPrograms Members
+
+        private VstProgramCollection _programs;
+        /// <summary>
+        /// Gets all the programs.
+        /// </summary>
+        public VstProgramCollection Programs
+        {
+            get
+            {
+                if (_programs == null)
+                {
+                    _programs = CreateProgramCollection();
+                }
+
+                return _programs;
+            }
+        }
+
+        private VstProgram _activeProgram;
+        /// <summary>
+        /// Gets or sets the current or active program.
+        /// </summary>
+        public virtual VstProgram ActiveProgram
+        {
+            get
+            {
+                if (_activeProgram == null && Programs.Count > 0)
+                {
+                    ActiveProgram = Programs[0];
+                }
+
+                return _activeProgram;
+            }
+            set
+            {
+                if (_activeProgram != value)
+                {
+                    if (_activeProgram != null)
+                    {
+                        _activeProgram.Parameters.Deactivate();
+                    }
+
+                    _activeProgram = value;
+
+                    if (_activeProgram != null)
+                    {
+                        _activeProgram.Parameters.Activate();
+                    }
+                }
+            }
+        }
+
+        public virtual void BeginSetProgram()
+        {
+        }
+
+        public virtual void EndSetProgram()
+        {
+        }
+
+        #endregion
+
+        protected abstract VstProgramCollection CreateProgramCollection();
+    }
+}
