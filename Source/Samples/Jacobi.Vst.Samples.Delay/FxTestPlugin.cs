558c2fe480cb8e6923b1b7f142b3019f450e9722
Marc Jacobi <obiwanjacobi@hotmail.com>
2009-03-01 14:08:52 +0000

    Rewritten the samples a bit to use the new base classes. Added custom peristence to the MidiNoteMapper. Also made the Host property public on the VstPlugin(WithInterfaceManager)Base classes.

--- Source/Samples/Jacobi.Vst.Samples.Delay/FxTestPlugin.cs
+++ Source/Samples/Jacobi.Vst.Samples.Delay/FxTestPlugin.cs
@@ -1,19 +1,23 @@
 ﻿namespace Jacobi.Vst.Samples.Delay
 {
+    using Jacobi.Vst.Core;
     using Jacobi.Vst.Framework;
+    using Jacobi.Vst.Framework.Plugin;
 
     /// <summary>
     /// The Plugin root class.
     /// </summary>
-    internal class FxTestPlugin : IVstPlugin
+    internal class FxTestPlugin : VstPluginBase
     {
-        private IVstHost _host;
         private FxPluginInterfaceManager _intfMgr;
         
         /// <summary>
         /// Constructs a new instance.
         /// </summary>
         public FxTestPlugin()
+            : base("VST.NET Delay Plugin", 
+                new VstProductInfo("VST.NET Code Samples", "Jacobi Software (c) 2009", 1000),
+                VstPluginCategory.RoomFx, VstPluginCapabilities.None, 0, 0x3A3A3A3A)
         {
             _intfMgr = new FxPluginInterfaceManager(this);
             ParameterFactory = new PluginParameterFactory();
@@ -24,97 +28,9 @@
         }
 
         /// <summary>
-        /// Gets or sets the parameter factory.
+        /// Gets the parameter factory.
         /// </summary>
-        public PluginParameterFactory ParameterFactory { get; set; }
-
-
-        #region IVstPlugin Members
-
-        private VstProductInfo _productInfo;
-        /// <summary>
-        /// Gets the product info.
-        /// </summary>
-        public VstProductInfo ProductInfo
-        {
-            get
-            {
-                if (_productInfo == null)
-                {
-                    _productInfo = new VstProductInfo("VST.NET Code Samples", "Jacobi Software (c) 2009", 1000);
-                }
-
-                return _productInfo;
-            }
-        }
-
-        /// <summary>
-        /// Gets the plugin name.
-        /// </summary>
-        public string Name
-        {
-            get { return "VST.NET Delay Plugin"; }
-        }
-
-        /// <summary>
-        /// Gets the plugin category
-        /// </summary>
-        public Jacobi.Vst.Core.VstPluginCategory Category
-        {
-            get { return Jacobi.Vst.Core.VstPluginCategory.RoomFx; }
-        }
-
-        /// <summary>
-        /// Gets the plugin additional capabilities.
-        /// </summary>
-        public VstPluginCapabilities Capabilities
-        {
-            get { return VstPluginCapabilities.None; }
-        }
-
-        /// <summary>
-        /// Gets the initial delay of the plugin.
-        /// </summary>
-        public int InitialDelay
-        {
-            get { return 0; }
-        }
-
-        /// <summary>
-        /// Gets the unique plugin ID.
-        /// </summary>
-        /// <remarks>A four character code as integer.</remarks>
-        public int PluginID
-        {
-            get { return 0x3A3A3A3A; }
-        }
-
-        /// <summary>
-        /// Opens the plugin and passes the host root interface.
-        /// </summary>
-        /// <param name="host">A framework implemented instance of the host root interface.</param>
-        public void Open(IVstHost host)
-        {
-            _host = host;
-        }
-
-        /// <summary>
-        /// Suspend plugin processing.
-        /// </summary>
-        public void Suspend()
-        {
-            
-        }
-
-        /// <summary>
-        /// Resume plugin processing.
-        /// </summary>
-        public void Resume()
-        {
-            
-        }
-
-        #endregion
+        public PluginParameterFactory ParameterFactory { get; private set; }
 
         #region IExtensibleObject Members
 
@@ -123,7 +39,7 @@
         /// </summary>
         /// <typeparam name="T">Type of an interface or class.</typeparam>
         /// <returns>Returns true if implemented, otherwise false is returned.</returns>
-        public bool Supports<T>() where T : class
+        public override bool Supports<T>()
         {
             return _intfMgr.Supports<T>();
         }
@@ -133,22 +49,23 @@
         /// </summary>
         /// <typeparam name="T">Type of an interface or class.</typeparam>
         /// <returns>Returns null of the type <typeparamref name="T"/> is not implemented.</returns>
-        public T GetInstance<T>() where T : class
+        public override T GetInstance<T>()
         {
             return _intfMgr.GetInstance<T>();
         }
 
         #endregion
-
+ 
         #region IDisposable Members
         /// <summary>
         /// Dispose the plugin.
         /// </summary>
-        public void Dispose()
+        public override void Dispose()
         {
             _intfMgr.Dispose();
             _intfMgr = null;
-            _host = null;
+
+            base.Dispose();
         }
 
         #endregion
--- Source/Samples/Jacobi.Vst.Samples.Delay/PluginPersistence.cs
+++ Source/Samples/Jacobi.Vst.Samples.Delay/PluginPersistence.cs
@@ -3,16 +3,19 @@
     using System.Diagnostics;
     using System.IO;
     using System.Text;
+
     using Jacobi.Vst.Core;
     using Jacobi.Vst.Framework;
+    using Jacobi.Vst.Framework.Plugin;
+    using Jacobi.Vst.Framework.Plugin.IO;
+    
 
     /// <summary>
     /// This class manages custom persistence for all the plugin's programs and parameters.
     /// </summary>
-    internal class PluginPersistence : IVstPluginPersistence
+    internal class PluginPersistence : VstPluginPersistenceBase
     {
         private FxTestPlugin _plugin;
-        private Encoding _encoding = Encoding.ASCII;
 
         /// <summary>
         /// Constructs a new instance.
@@ -23,116 +26,29 @@
             _plugin = plugin;
         }
 
-        #region IVstPluginPersistence Members
-
-        /// <summary>
-        /// Reads the programs for the <paramref name="stream"/> and fills the <paramref name="programs"/> collection.
-        /// </summary>
-        /// <param name="stream">Must not be null.</param>
-        /// <param name="programs">Must not be null.</param>
-        public void ReadPrograms(Stream stream, VstProgramCollection programs)
+        protected override VstProgramReaderBase CreateProgramReader(Stream input)
         {
-            BinaryReader reader = new BinaryReader(stream, _encoding);
-
-            int count = reader.ReadInt32();
-
-            for(int i = 0; i < count; i++)
-            {
-                VstProgram program = ReadProgram(reader);
-
-                programs.Add(program);
-            }
+            return new DelayProgramReader(_plugin, input, Encoding);
         }
 
-        /// <summary>
-        /// Writes all the <paramref name="programs"/> to the <paramref name="stream"/>.
-        /// </summary>
-        /// <param name="stream">Must not be null.</param>
-        /// <param name="programs">Must not be null.</param>
-        public void WritePrograms(Stream stream, VstProgramCollection programs)
+        private class DelayProgramReader : VstProgramReaderBase
         {
-            BinaryWriter writer = new BinaryWriter(stream, _encoding);
+            private FxTestPlugin _plugin;
 
-            writer.Write(programs.Count);
-
-            foreach (VstProgram program in programs)
+            public DelayProgramReader(FxTestPlugin plugin, Stream input, Encoding encoding)
+                : base(input, encoding)
             {
-                WriteProgram(writer, program);
-            }
-        }
-
-        /// <summary>
-        /// Called just before reading in programs to detect (in)compatibility.
-        /// </summary>
-        /// <param name="chunkInfo">Must not be null.</param>
-        /// <returns>Returns true when the data can be read.</returns>
-        public bool CanLoadChunk(VstPatchChunkInfo chunkInfo)
-        {
-            // TODO: determine version
-            return true;
-        }
-
-        #endregion
-
-        /// <summary>
-        /// Helper method to read one program and its parameters.
-        /// </summary>
-        /// <param name="reader">Must not be null.</param>
-        /// <returns>Never returns null.</returns>
-        private VstProgram ReadProgram(BinaryReader reader)
-        {
-            VstProgram program = CreateProgram();
-
-            program.Name = reader.ReadString();
-            int paramCount = reader.ReadInt32();
-
-            for (int i = 0; i < paramCount; i++ )
-            {
-                string paramName = reader.ReadString();
-
-                if (program.Parameters.Contains(paramName))
-                {
-                    VstParameter parameter = program.Parameters[paramName];
-                    parameter.Value = reader.ReadSingle();
-                }
-                else
-                {
-                    // keep stream in sync.
-                    reader.ReadSingle();
-                }
+                _plugin = plugin;
             }
 
-            return program;
-        }
+            protected override VstProgram CreateProgram()
+            {    
+                VstProgram program = new VstProgram(_plugin.ParameterFactory.Categories);
 
-        /// <summary>
-        /// Helper method to write one program and its parameters.
-        /// </summary>
-        /// <param name="writer">Must not be null.</param>
-        /// <param name="program">Must not be null.</param>
-        private void WriteProgram(BinaryWriter writer, VstProgram program)
-        {
-            writer.Write(program.Name);
-            writer.Write(program.Parameters.Count);
+                _plugin.ParameterFactory.CreateParameters(program.Parameters);
 
-            foreach (VstParameter parameter in program.Parameters)
-            {
-                writer.Write(parameter.Info.Name);
-                writer.Write(parameter.Value);
+                return program;
             }
-        }
-
-        /// <summary>
-        /// Helper method to create a new program and its parameters.
-        /// </summary>
-        /// <returns>Never returns null.</returns>
-        private VstProgram CreateProgram()
-        {
-            VstProgram program = new VstProgram(_plugin.ParameterFactory.Categories);
-
-            _plugin.ParameterFactory.CreateParameters(program.Parameters);
-
-            return program;
         }
     }
 }
