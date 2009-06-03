9909fdd5a72a142e0d1de7d0456b654956d06369
Marc Jacobi <obiwanjacobi@hotmail.com>
2009-06-03 19:04:41 +0000

    Ported the Delay sample to VB.NET

---
+++ Source/Samples/Jacobi.Vst.Samples.Delay.VB/PluginPersistence.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Diagnostics
+Imports System.IO
+Imports System.Text
+
+Imports Jacobi.Vst.Core
+Imports Jacobi.Vst.Framework
+Imports Jacobi.Vst.Framework.Plugin
+Imports Jacobi.Vst.Framework.Plugin.IO
+
+Namespace Jacobi.Vst.Samples.Delay.VB
+
+    Friend Class PluginPersistence
+        Inherits VstPluginPersistenceBase
+
+        Private _plugin As FxTestPlugin
+
+        Public Sub New(ByVal plugin As FxTestPlugin)
+            _plugin = plugin
+        End Sub
+
+        Protected Overrides Function CreateProgramReader(ByVal input As Stream) As VstProgramReaderBase
+            Return New DelayProgramReader(_plugin, input, Encoding)
+        End Function
+
+        Private Class DelayProgramReader
+            Inherits VstProgramReaderBase
+
+            Private _plugin As FxTestPlugin
+
+            Public Sub New(ByVal plugin As FxTestPlugin, ByVal input As Stream, ByVal encoding As Encoding)
+                MyBase.New(input, encoding)
+
+                _plugin = plugin
+            End Sub
+
+            Protected Overrides Function CreateProgram() As Framework.VstProgram
+                Dim program As VstProgram = New VstProgram(_plugin.ParameterFactory.Categories)
+
+                _plugin.ParameterFactory.CreateParameters(program.Parameters)
+
+                Return program
+            End Function
+
+        End Class
+    End Class
+
+End Namespace
---
+++ Source/Samples/Jacobi.Vst.Samples.Delay.VB/TestPluginCommandStub.vb
@@ -0,0 +1,18 @@
+﻿Imports Jacobi.Vst.Framework
+Imports Jacobi.Vst.Framework.Plugin
+
+Namespace Jacobi.Vst.Samples.Delay.VB
+
+    Public Class TestPluginCommandStub
+        Inherits StdPluginCommandStub
+        Implements Core.Plugin.IVstPluginCommandStub
+
+        Protected Overrides Function CreatePluginInstance() As Framework.IVstPlugin
+
+            Return New FxTestPlugin()
+
+        End Function
+
+    End Class
+
+End Namespace
