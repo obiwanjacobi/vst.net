ceef10cb13e164d90e02c2944c46813b4d63f59a
Marc Jacobi <obiwanjacobi@hotmail.com>
2009-06-03 19:04:41 +0000

    Ported the Delay sample to VB.NET

---
+++ Source/Samples/Jacobi.Vst.Samples.Delay.VB/PluginPrograms.vb
@@ -0,0 +1,41 @@
+﻿Imports Jacobi.Vst.Framework
+Imports Jacobi.Vst.Framework.Plugin
+
+Namespace Jacobi.Vst.Samples.Delay.VB
+
+    Friend Class PluginPrograms
+        Inherits VstPluginProgramsBase
+
+        Private _plugin As FxTestPlugin
+
+        Public Sub New(ByVal plugin As FxTestPlugin)
+            _plugin = plugin
+        End Sub
+
+        Protected Overrides Function CreateProgramCollection() As Framework.VstProgramCollection
+            Dim programs As VstProgramCollection = New VstProgramCollection()
+
+            Dim prog As VstProgram = New VstProgram(_plugin.ParameterFactory.Categories)
+            prog.Name = "Fx Program 1"
+            _plugin.ParameterFactory.CreateParameters(prog.Parameters)
+
+            programs.Add(prog)
+
+            prog = New VstProgram(_plugin.ParameterFactory.Categories)
+            prog.Name = "Fx Program 2"
+            _plugin.ParameterFactory.CreateParameters(prog.Parameters)
+
+            programs.Add(prog)
+
+            prog = New VstProgram(_plugin.ParameterFactory.Categories)
+            prog.Name = "Fx Program 3"
+            _plugin.ParameterFactory.CreateParameters(prog.Parameters)
+
+            programs.Add(prog)
+
+            Return programs
+        End Function
+
+    End Class
+
+End Namespace
