ca6a87a3d4ad3d51839e78f0547cff05a7cae348
obiwanjacobi_cp <SND\obiwanjacobi_cp>
2009-06-03 19:04:41 +0000

    Ported the Delay sample to VB.NET

---
+++ Source/Samples/Jacobi.Vst.Samples.Delay.VB/FxPluginInterfaceManager.vb
@@ -0,0 +1,39 @@
+﻿Imports Jacobi.Vst.Framework.Plugin
+
+Namespace Jacobi.Vst.Samples.Delay.VB
+
+    Friend Class FxPluginInterfaceManager
+        Inherits PluginInterfaceManagerBase
+
+        Private _plugin As FxTestPlugin
+
+        Public Sub New(ByVal plugin As FxTestPlugin)
+            _plugin = plugin
+        End Sub
+
+        Protected Overrides Function CreateAudioProcessor(ByVal instance As Framework.IVstPluginAudioProcessor) As Framework.IVstPluginAudioProcessor
+            If instance Is Nothing Then
+                Return New AudioProcessor(_plugin)
+            End If
+
+            Return instance
+        End Function
+
+        Protected Overrides Function CreatePrograms(ByVal instance As Framework.IVstPluginPrograms) As Framework.IVstPluginPrograms
+            If instance Is Nothing Then
+                Return New PluginPrograms(_plugin)
+            End If
+
+            Return instance
+        End Function
+
+        Protected Overrides Function CreatePersistence(ByVal instance As Framework.IVstPluginPersistence) As Framework.IVstPluginPersistence
+            If instance Is Nothing Then
+                Return New PluginPersistence(_plugin)
+            End If
+
+            Return instance
+        End Function
+    End Class
+
+End Namespace
