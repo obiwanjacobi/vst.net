1ad65680d027281e6b0cb0bebd28fcb251349608
obiwanjacobi_cp <SND\obiwanjacobi_cp>
2009-07-03 08:55:48 +0000

    Added support for the deprecated method WantMidi on the HostCommandStub. The Framework now contains an extra IVstHostDeprecated interface a plugin can use to call deprecated methods. The PluginBase(WithInterfaceManager) base classes now implement the Resume method to call WantMidi when the Plugin implements the IVstMidiProcessor.

---
+++ Source/Code/Jacobi.Vst.Framework/Host/VstHostDeprecated.cs
@@ -0,0 +1,41 @@
+﻿namespace Jacobi.Vst.Framework.Host
+{
+    using Jacobi.Vst.Core;
+
+    /// <summary>
+    /// Forwards the calls of the <see cref="IVstDeprecated"/> interface to the Host Command Stub.
+    /// </summary>
+    internal class VstHostDeprecated : IVstHostDeprecated
+    {
+        private VstHost _host;
+
+        /// <summary>
+        /// Constructs an instance on the host proxy.
+        /// </summary>
+        /// <param name="host">Must not be null.</param>
+        /// <exception cref="ArgumentNullException">Thrown when <paramref name="host"/> is not set to an instance of an object.</exception>
+        public VstHostDeprecated(VstHost host)
+        {
+            Throw.IfArgumentIsNull(host, "host");
+
+            _host = host;
+        }
+
+        #region IVstHostDeprecated Members
+
+        public bool WantMidi()
+        {
+            IVstHostCommandsDeprecated deprecatedCmds = _host.HostCommandStub as IVstHostCommandsDeprecated;
+
+            if (deprecatedCmds != null)
+            {
+                deprecatedCmds.WantMidi();
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+    }
+}
