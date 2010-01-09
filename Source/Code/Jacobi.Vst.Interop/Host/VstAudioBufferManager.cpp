e5932ddd1cf0892672e451ba56ff672abb00ffea
Marc Jacobi <obiwanjacobi@hotmail.com>
2010-01-09 17:58:09 +0000

    Fixes for (Interop.Host).IVstPluginCommandStub.ProcessReplacing implementation. Both no audio buffers and more than 2 buffers now work ok.

--- Source/Code/Jacobi.Vst.Interop/Host/VstAudioBufferManager.cpp
+++ Source/Code/Jacobi.Vst.Interop/Host/VstAudioBufferManager.cpp
@@ -29,10 +29,10 @@ namespace Host
 			// allocate the buffers in one call
 			float* pBuffer = _unmanagedBuffers.GetArray(bufferCount * bufferSize);
 
-			for(int n = 1; n <= bufferCount; n++)
+			for(int n = 0; n < bufferCount; n++)
 			{
 				_managedBuffers->Add(gcnew Jacobi::Vst::Core::VstAudioBuffer(pBuffer, bufferSize, true));
-				pBuffer += n * bufferSize;
+				pBuffer += bufferSize;
 			}
 		}
 	}
