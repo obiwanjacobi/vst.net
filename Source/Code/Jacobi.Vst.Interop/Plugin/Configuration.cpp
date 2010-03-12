8cb3cb1f74e49f6e59b0c865c6848767cf6814bb
Marc Jacobi <obiwanjacobi@hotmail.com>
2010-03-12 20:03:40 +0000

    Added per plugin config file support. Changed the IVstPluginCommandStub interface (Core.Plugin): it now has a property that receives the plugin Configuration object. Probe paths in the plugin config file (that must be next to the renamed Interop assembly) are considered for assembly resolvement.

---
+++ Source/Code/Jacobi.Vst.Interop/Plugin/Configuration.cpp
@@ -0,0 +1,19 @@
+#include "StdAfx.h"
+#include "Configuration.h"
+
+namespace Jacobi {
+namespace Vst {
+namespace Interop {
+namespace Plugin {
+
+Configuration::Configuration(System::String^ exeFilePath)
+{
+	_config = System::Configuration::ConfigurationManager::OpenExeConfiguration(exeFilePath);
+
+	if(_config != nullptr && !_config->HasFile)
+	{
+		_config = nullptr;
+	}
+}
+
+}}}} // Jacobi::Vst::Interop::Plugin
