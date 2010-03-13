21bda921b532cec88f787f5896d046efb7bd8ff2
Marc Jacobi <obiwanjacobi@hotmail.com>
2010-03-13 16:16:16 +0000

    Added the wrapper plugin sample.

---
+++ Source/Samples/Jacobi.Vst.Samples.WrapperPlugin/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Jacobi.Vst.Samples.WrapperPlugin")]
+[assembly: AssemblyDescription("A plugin that wraps another plugin.")]
+[assembly: AssemblyProduct("Jacobi.Vst.Samples.WrapperPlugin")]
+[assembly: AssemblyCulture("")]
