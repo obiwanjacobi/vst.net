75fc8a7572b591383e24f7dedbcf86784e84df61
Marc Jacobi <obiwanjacobi@hotmail.com>
2010-05-04 17:47:40 +0000

    Added code comments and started a unit test for the observables (Framework).

--- Source/Code/Jacobi.Vst.Framework/Common/NotifyCollectionChangedBase.cs
+++ Source/Code/Jacobi.Vst.Framework/Common/NotifyCollectionChangedBase.cs
@@ -3,11 +3,21 @@ using System.Collections;
 
 namespace Jacobi.Vst.Framework.Common
 {
+    /// <summary>
+    /// A base class that helps a collection implement the <see cref="INotifyCollectionChanged"/> interface.
+    /// </summary>
     public abstract class NotifyCollectionChangedBase : INotifyCollectionChanged
     {
+        /// <inheritdoc/>
         public event EventHandler<NotifyCollectionChangedEventArgs> CollectionChanged;
 
-        protected void OnCollectionChanged(NotifyColletionChangedAction action, object newItem, object oldItem)
+        /// <summary>
+        /// Fires the <see cref="CollectionChanged"/> event using single instances.
+        /// </summary>
+        /// <param name="action">The type of action that caused the collection to change.</param>
+        /// <param name="newItem">Can be null.</param>
+        /// <param name="oldItem">Can be null.</param>
+        protected void OnCollectionChanged(NotifyCollectionChangedAction action, object newItem, object oldItem)
         {
             var handler = CollectionChanged;
 
@@ -17,7 +27,13 @@ namespace Jacobi.Vst.Framework.Common
             }
         }
 
-        protected void OnCollectionChanged(NotifyColletionChangedAction action, IList newItems, IList oldItems)
+        /// <summary>
+        /// Fires the <see cref="CollectionChanged"/> event using lists of items.
+        /// </summary>
+        /// <param name="action">The type of action that caused the collection to change.</param>
+        /// <param name="newItems">Can be null.</param>
+        /// <param name="oldItems">Can be null.</param>
+        protected void OnCollectionChanged(NotifyCollectionChangedAction action, IList newItems, IList oldItems)
         {
             var handler = CollectionChanged;
 
--- Source/Code/Jacobi.Vst.Framework/Common/ObservableCollection.cs
+++ Source/Code/Jacobi.Vst.Framework/Common/ObservableCollection.cs
@@ -4,47 +4,62 @@ using System.Collections.ObjectModel;
 
 namespace Jacobi.Vst.Framework.Common
 {
+    /// <summary>
+    /// A collection class that implements the <see cref="INotifyCollectionChanged"/> interface.
+    /// </summary>
+    /// <typeparam name="T">The type of collection items.</typeparam>
     public class ObservableCollection<T> : Collection<T>, INotifyCollectionChanged
     {
+        /// <inheritdoc/>
         protected override void InsertItem(int index, T item)
         {
             base.InsertItem(index, item);
 
-            OnCollectionChanged(NotifyColletionChangedAction.Add, item, null);
+            OnCollectionChanged(NotifyCollectionChangedAction.Add, item, null);
         }
 
+        /// <inheritdoc/>
         protected override void RemoveItem(int index)
         {
             T oldItem = this[index];
 
             base.RemoveItem(index);
 
-            OnCollectionChanged(NotifyColletionChangedAction.Remove, null, oldItem);
+            OnCollectionChanged(NotifyCollectionChangedAction.Remove, null, oldItem);
         }
 
+        /// <inheritdoc/>
         protected override void SetItem(int index, T item)
         {
             T oldItem = this[index];
 
             base.SetItem(index, item);
 
-            OnCollectionChanged(NotifyColletionChangedAction.Replace, item, oldItem);
+            OnCollectionChanged(NotifyCollectionChangedAction.Replace, item, oldItem);
         }
 
+        /// <inheritdoc/>
         protected override void ClearItems()
         {
             ArrayList oldItems = new ArrayList(this);
 
             base.ClearItems();
 
-            OnCollectionChanged(NotifyColletionChangedAction.Reset, null, oldItems);
+            OnCollectionChanged(NotifyCollectionChangedAction.Reset, null, oldItems);
         }
 
         #region INotifyCollectionChanged Members
 
+        /// <inheritdoc/>
         public event EventHandler<NotifyCollectionChangedEventArgs> CollectionChanged;
 
-        protected void OnCollectionChanged(NotifyColletionChangedAction action, object newItem, object oldItem)
+        /// <summary>
+        /// Fires the <see cref="CollectionChanged"/> event using single instances.
+        /// </summary>
+        /// <param name="action">The type of action that caused the collection to change.</param>
+        /// <param name="newItem">Can be null.</param>
+        /// <param name="oldItem">Can be null.</param>
+        protected void OnCollectionChanged(NotifyCollectionChangedAction action, object newItem, object oldItem)
         {
             var handler = CollectionChanged;
 
@@ -54,7 +69,13 @@ namespace Jacobi.Vst.Framework.Common
             }
         }
 
-        protected void OnCollectionChanged(NotifyColletionChangedAction action, IList newItems, IList oldItems)
+        /// <summary>
+        /// Fires the <see cref="CollectionChanged"/> event using lists of items.
+        /// </summary>
+        /// <param name="action">The type of action that caused the collection to change.</param>
+        /// <param name="newItems">Can be null.</param>
+        /// <param name="oldItems">Can be null.</param>
+        protected void OnCollectionChanged(NotifyCollectionChangedAction action, IList newItems, IList oldItems)
         {
             var handler = CollectionChanged;
 
