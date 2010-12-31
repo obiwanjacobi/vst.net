c9a036107c33ca93bcf1cc4d75036bbdd85703b2
obiwanjacobi_cp <SND\obiwanjacobi_cp>
2010-12-31 21:44:26 +0000

    Document update and version bump for release. Fixed namespaces of VB sample.

--- Source/Samples/Jacobi.Vst.Samples.Delay.VB/PluginParameterFactory.vb
+++ Source/Samples/Jacobi.Vst.Samples.Delay.VB/PluginParameterFactory.vb
@@ -1,29 +1,25 @@
 ﻿Imports Jacobi.Vst.Core
 Imports Jacobi.Vst.Framework
 
-Namespace Jacobi.Vst.Samples.Delay.VB
+Friend Class PluginParameterFactory
+    Public Categories As VstParameterCategoryCollection = New VstParameterCategoryCollection()
+    Public ParameterInfos As VstParameterInfoCollection = New VstParameterInfoCollection()
 
-    Friend Class PluginParameterFactory
-        Public Categories As VstParameterCategoryCollection = New VstParameterCategoryCollection()
-        Public ParameterInfos As VstParameterInfoCollection = New VstParameterInfoCollection()
+    Public Sub New()
+        Dim paramCat As VstParameterCategory = New VstParameterCategory()
+        paramCat.Name = "Delay"
 
-        Public Sub New()
-            Dim paramCat As VstParameterCategory = New VstParameterCategory()
-            paramCat.Name = "Delay"
+        Categories.Add(paramCat)
+    End Sub
 
-            Categories.Add(paramCat)
-        End Sub
+    Public Sub CreateParameters(ByVal parameters As VstParameterCollection)
+        For Each paramInfo As VstParameterInfo In ParameterInfos
+            If Categories.Count > 0 And paramInfo.Category Is Nothing Then paramInfo.Category = Categories(0)
 
-        Public Sub CreateParameters(ByVal parameters As VstParameterCollection)
-            For Each paramInfo As VstParameterInfo In ParameterInfos
-                If Categories.Count > 0 And paramInfo.Category Is Nothing Then paramInfo.Category = Categories(0)
+            Dim param As VstParameter = New VstParameter(paramInfo)
 
-                Dim param As VstParameter = New VstParameter(paramInfo)
+            parameters.Add(param)
+        Next
+    End Sub
 
-                parameters.Add(param)
-            Next
-        End Sub
-
-    End Class
-
-End Namespace
+End Class
--- Source/Samples/Jacobi.Vst.Samples.Delay.VB/PluginPrograms.vb
+++ Source/Samples/Jacobi.Vst.Samples.Delay.VB/PluginPrograms.vb
@@ -1,41 +1,37 @@
 ﻿Imports Jacobi.Vst.Framework
 Imports Jacobi.Vst.Framework.Plugin
 
-Namespace Jacobi.Vst.Samples.Delay.VB
+Friend Class PluginPrograms
+    Inherits VstPluginProgramsBase
 
-    Friend Class PluginPrograms
-        Inherits VstPluginProgramsBase
+    Private _plugin As FxTestPlugin
 
-        Private _plugin As FxTestPlugin
+    Public Sub New(ByVal plugin As FxTestPlugin)
+        _plugin = plugin
+    End Sub
 
-        Public Sub New(ByVal plugin As FxTestPlugin)
-            _plugin = plugin
-        End Sub
+    Protected Overrides Function CreateProgramCollection() As Framework.VstProgramCollection
+        Dim programs As VstProgramCollection = New VstProgramCollection()
 
-        Protected Overrides Function CreateProgramCollection() As Framework.VstProgramCollection
-            Dim programs As VstProgramCollection = New VstProgramCollection()
+        Dim prog As VstProgram = New VstProgram(_plugin.ParameterFactory.Categories)
+        prog.Name = "Fx Program 1"
+        _plugin.ParameterFactory.CreateParameters(prog.Parameters)
 
-            Dim prog As VstProgram = New VstProgram(_plugin.ParameterFactory.Categories)
-            prog.Name = "Fx Program 1"
-            _plugin.ParameterFactory.CreateParameters(prog.Parameters)
+        programs.Add(prog)
 
-            programs.Add(prog)
+        prog = New VstProgram(_plugin.ParameterFactory.Categories)
+        prog.Name = "Fx Program 2"
+        _plugin.ParameterFactory.CreateParameters(prog.Parameters)
 
-            prog = New VstProgram(_plugin.ParameterFactory.Categories)
-            prog.Name = "Fx Program 2"
-            _plugin.ParameterFactory.CreateParameters(prog.Parameters)
+        programs.Add(prog)
 
-            programs.Add(prog)
+        prog = New VstProgram(_plugin.ParameterFactory.Categories)
+        prog.Name = "Fx Program 3"
+        _plugin.ParameterFactory.CreateParameters(prog.Parameters)
 
-            prog = New VstProgram(_plugin.ParameterFactory.Categories)
-            prog.Name = "Fx Program 3"
-            _plugin.ParameterFactory.CreateParameters(prog.Parameters)
+        programs.Add(prog)
 
-            programs.Add(prog)
+        Return programs
+    End Function
 
-            Return programs
-        End Function
-
-    End Class
-
-End Namespace
+End Class
