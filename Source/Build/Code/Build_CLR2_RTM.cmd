ef0aac940a6cf48f6723affff494bc3cc8556337
obiwanjacobi_cp <SND\obiwanjacobi_cp>
2012-05-10 08:30:40 +0000

    Finalized build automation support.

--- Source/Build/Code/Build_CLR2_RTM.cmd
+++ Source/Build/Code/Build_CLR2_RTM.cmd
@@ -1,12 +1,13 @@
 CALL "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 
 CALL Clean_CLR2.cmd
-
 Msbuild /p:Configuration=Debug /p:Platform="Any CPU (RTM)" ..\..\Code\Jacobi.Vst.sln
 Msbuild /p:Configuration=Release /p:Platform="Any CPU (RTM)" ..\..\Code\Jacobi.Vst.sln
 
+CALL Clean_CLR2.cmd
 Msbuild /p:Configuration=Debug /p:Platform="x86" ..\..\Code\Jacobi.Vst.sln
 Msbuild /p:Configuration=Release /p:Platform="x86" ..\..\Code\Jacobi.Vst.sln
 
+CALL Clean_CLR2.cmd
 Msbuild /p:Configuration=Debug /p:Platform="x64" ..\..\Code\Jacobi.Vst.sln
 Msbuild /p:Configuration=Release /p:Platform="x64" ..\..\Code\Jacobi.Vst.sln
