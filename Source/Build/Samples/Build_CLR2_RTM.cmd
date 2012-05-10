56f0561d39c9f49d802b5cbbbd0eb9ce208c2bef
Marc Jacobi <obiwanjacobi@hotmail.com>
2012-05-10 08:30:40 +0000

    Finalized build automation support.

--- Source/Build/Samples/Build_CLR2_RTM.cmd
+++ Source/Build/Samples/Build_CLR2_RTM.cmd
@@ -1,13 +1,14 @@
 CALL "c:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 
 REM Build dependencies
+CALL ..\Code\Clean_CLR2.cmd
 Msbuild /t:Clean /p:Configuration=Debug /p:Platform="Any CPU (RTM)" ..\..\Code\Jacobi.Vst.sln
 Msbuild /t:Clean /p:Configuration=Release /p:Platform="Any CPU (RTM)" ..\..\Code\Jacobi.Vst.sln
 
+CALL ..\Code\Clean_CLR2.cmd
 Msbuild /p:Configuration=Debug /p:Platform="Any CPU (RTM)" ..\..\Code\Jacobi.Vst.sln
 Msbuild /p:Configuration=Release /p:Platform="Any CPU (RTM)" ..\..\Code\Jacobi.Vst.sln
 
 CALL Clean_CLR2.cmd
-
 Msbuild /p:Configuration=Debug /p:Platform="Any CPU" ..\..\Samples\Jacobi.Vst.Samples.sln
 Msbuild /p:Configuration=Release /p:Platform="Any CPU" ..\..\Samples\Jacobi.Vst.Samples.sln
