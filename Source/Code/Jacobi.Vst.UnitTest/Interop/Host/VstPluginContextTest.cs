3f48ebdeb79c5ed4cdf26887b05af5ecfffbf03b
Marc Jacobi <obiwanjacobi@hotmail.com>
2014-09-13 06:11:35 +0000

    Fixed Interop embedded resource bug.

---
+++ Source/Code/Jacobi.Vst.UnitTest/Interop/Host/VstPluginContextTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jacobi.Vst.Interop.Host;
+using System.Reflection;
+
+namespace Jacobi.Vst.UnitTest.Interop.Host
+{
+    [TestClass]
+    public class VstPluginContextTest
+    {
+        public TestContext TestContext;
+
+        [WorkItem(10484)]
+        [WorkItem(8488)]
+        [TestMethod]
+        [ExpectedException(typeof(EntryPointNotFoundException))]
+        public void Create_InvalidPluginFile_ThrowsExpectedExceptionWithResourceString()
+        {
+            var hostCmdStub = new StubHostCommandStub();
+            var notaPluginFile = Assembly.GetExecutingAssembly().Location;
+
+            VstPluginContext.Create(notaPluginFile, hostCmdStub);
+        }
+    }
+}
