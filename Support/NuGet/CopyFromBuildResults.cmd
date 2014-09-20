3ac59e86cd3e38bf23a9ee8f61ddeecc0b13bd3c
Marc Jacobi <obiwanjacobi@hotmail.com>
2014-09-20 20:57:33 +0000

    Added NuGet package build scripts

---
+++ Support/NuGet/CopyFromBuildResults.cmd
@@ -0,0 +1,2 @@
+xcopy "..\..\Source\BuildResults\CLR4\x86\Release" "VST.NET %1\x86\lib\net40\" /F /Y /EXCLUDE:Copy_Excludes.txt
+xcopy "..\..\Source\BuildResults\CLR4\x64\Release" "VST.NET %1\x64\lib\net40\" /F /Y /EXCLUDE:Copy_Excludes.txt
