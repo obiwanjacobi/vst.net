0b26d12afea95e33c864aa51bf4bc78b562facb3
obiwanjacobi_cp <SND\obiwanjacobi_cp>
2014-09-20 12:36:21 +0000

    More build stuff

---
+++ Source/Build/Build_CLR2.cmd
@@ -0,0 +1,11 @@
+CALL BuildSolution Debug x86 ..\Code\Jacobi.Vst.Clr2.sln
+CALL BuildSolution Debug x86 ..\Samples\Jacobi.Vst.Samples.Clr2.sln
+
+CALL BuildSolution Release x86 ..\Code\Jacobi.Vst.Clr2.sln
+CALL BuildSolution Release x86 ..\Samples\Jacobi.Vst.Samples.Clr2.sln
+
+CALL BuildSolution Debug x64 ..\Code\Jacobi.Vst.Clr2.sln
+CALL BuildSolution Debug x64 ..\Samples\Jacobi.Vst.Samples.Clr2.sln
+
+CALL BuildSolution Release x64 ..\Code\Jacobi.Vst.Clr2.sln
+CALL BuildSolution Release x64 ..\Samples\Jacobi.Vst.Samples.Clr2.sln
---
+++ Source/Build/Build_CLR4.cmd
@@ -0,0 +1,11 @@
+CALL BuildSolution Debug x86 ..\Code\Jacobi.Vst.Clr4.sln
+CALL BuildSolution Debug x86 ..\Samples\Jacobi.Vst.Samples.Clr4.sln
+
+CALL BuildSolution Release x86 ..\Code\Jacobi.Vst.Clr4.sln
+CALL BuildSolution Release x86 ..\Samples\Jacobi.Vst.Samples.Clr4.sln
+
+CALL BuildSolution Debug x64 ..\Code\Jacobi.Vst.Clr4.sln
+CALL BuildSolution Debug x64 ..\Samples\Jacobi.Vst.Samples.Clr4.sln
+
+CALL BuildSolution Release x64 ..\Code\Jacobi.Vst.Clr4.sln
+CALL BuildSolution Release x64 ..\Samples\Jacobi.Vst.Samples.Clr4.sln
