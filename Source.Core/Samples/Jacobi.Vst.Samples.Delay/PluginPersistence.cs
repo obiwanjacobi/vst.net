917172aeabef44a1d375d1cbce6304983946139c
Marc Jacobi <marc.jacobi@macaw.nl>
2020-02-22 08:48:34 +0100

    ignore _sharedassemblies

---
+++ Source.Core/Samples/Jacobi.Vst.Samples.Delay/PluginPersistence.cs
@@ -0,0 +1,54 @@
+﻿namespace Jacobi.Vst.Samples.Delay
+{
+    using System.Diagnostics;
+    using System.IO;
+    using System.Text;
+
+    using Jacobi.Vst.Core;
+    using Jacobi.Vst.Framework;
+    using Jacobi.Vst.Framework.Plugin;
+    using Jacobi.Vst.Framework.Plugin.IO;
+    
+
+    /// <summary>
+    /// This class manages custom persistence for all the plugin's programs and parameters.
+    /// </summary>
+    internal class PluginPersistence : VstPluginPersistenceBase
+    {
+        private Plugin _plugin;
+
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        /// <param name="plugin">Must not be null.</param>
+        public PluginPersistence(Plugin plugin)
+        {
+            _plugin = plugin;
+        }
+
+        protected override VstProgramReaderBase CreateProgramReader(Stream input)
+        {
+            return new DelayProgramReader(_plugin, input, Encoding);
+        }
+
+        private class DelayProgramReader : VstProgramReaderBase
+        {
+            private Plugin _plugin;
+
+            public DelayProgramReader(Plugin plugin, Stream input, Encoding encoding)
+                : base(input, encoding)
+            {
+                _plugin = plugin;
+            }
+
+            protected override VstProgram CreateProgram()
+            {    
+                VstProgram program = new VstProgram(_plugin.ParameterFactory.Categories);
+
+                _plugin.ParameterFactory.CreateParameters(program.Parameters);
+
+                return program;
+            }
+        }
+    }
+}
---
+++ Source.Core/Samples/Jacobi.Vst.Samples.Delay/PluginPrograms.cs
@@ -0,0 +1,51 @@
+﻿namespace Jacobi.Vst.Samples.Delay
+{
+    using Jacobi.Vst.Framework;
+    using Jacobi.Vst.Framework.Plugin;
+
+    /// <summary>
+    /// This class manages the plugin programs.
+    /// </summary>
+    class PluginPrograms : VstPluginProgramsBase
+    {
+        Plugin _plugin;
+
+        /// <summary>
+        /// Constructs a new instance.
+        /// </summary>
+        /// <param name="plugin">Must not be null.</param>
+        public PluginPrograms(Plugin plugin)
+        {
+            _plugin = plugin;
+        }
+
+        /// <summary>
+        /// Initializes the plugin program collection.
+        /// </summary>
+        /// <returns>A filled program collection.</returns>
+        protected override VstProgramCollection CreateProgramCollection()
+        {
+            VstProgramCollection programs = new VstProgramCollection();
+
+            VstProgram prog = new VstProgram(_plugin.ParameterFactory.Categories);
+            prog.Name = "Fx Program 1";
+            _plugin.ParameterFactory.CreateParameters(prog.Parameters);
+
+            programs.Add(prog);
+
+            prog = new VstProgram(_plugin.ParameterFactory.Categories);
+            prog.Name = "Fx Program 2";
+            _plugin.ParameterFactory.CreateParameters(prog.Parameters);
+
+            programs.Add(prog);
+
+            prog = new VstProgram(_plugin.ParameterFactory.Categories);
+            prog.Name = "Fx Program 3";
+            _plugin.ParameterFactory.CreateParameters(prog.Parameters);
+
+            programs.Add(prog);
+
+            return programs;
+        }
+    }
+}
