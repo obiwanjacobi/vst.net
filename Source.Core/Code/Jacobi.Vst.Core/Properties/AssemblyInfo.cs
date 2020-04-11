618748d1a97a0f6491a0eb0199ae19118246703b
Marc Jacobi <marc.jacobi@macaw.nl>
2020-04-11 11:40:21 +0200

    nuget

--- Source.Core/Code/Jacobi.Vst.Core/Properties/AssemblyInfo.cs
+++ Source.Core/Code/Jacobi.Vst.Core/Properties/AssemblyInfo.cs
@@ -5,10 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyDescription("Core managed types.")]
+[assembly: AssemblyDescription("VST.NET Core managed types.")]
 [assembly: AssemblyCulture("")]
-
-// The core assembly is not CLS compliant.
 [assembly: CLSCompliant(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
