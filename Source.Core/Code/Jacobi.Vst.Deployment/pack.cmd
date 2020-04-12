7228a949d0515a006350d069d88021c86bb3d056
Marc Jacobi <marc.jacobi@macaw.nl>
2020-04-12 13:23:55 +0200

    nuget

--- Source.Core/Code/Jacobi.Vst.Deployment/pack.cmd
+++ Source.Core/Code/Jacobi.Vst.Deployment/pack.cmd
@@ -1,13 +1,13 @@
 :: x86
-xcopy "..\Release\Jacobi.*" "lib\netcore\" /F /Y
-xcopy "..\Jacobi.Vst.Core\bin\x86\Release\netcoreapp3.1\Jacobi.*" "lib\netcore\" /F /Y
-xcopy "..\Jacobi.Vst.Framework\bin\x86\Release\netcoreapp3.1\Jacobi.*" "lib\netcore\" /F /Y
+xcopy "..\Release\Jacobi.*" "runtimes\win10-x86\lib\netcoreapp3.1\" /F /Y
+xcopy "..\Jacobi.Vst.Core\bin\x86\Release\netcoreapp3.1\Jacobi.*" "runtimes\win10-x86\lib\netcoreapp3.1\" /F /Y
+xcopy "..\Jacobi.Vst.Framework\bin\x86\Release\netcoreapp3.1\Jacobi.*" "runtimes\win10-x86\lib\netcoreapp3.1\" /F /Y
 xcopy "..\Release\ijwhost.dll" "runtimes\win10-x86\native\" /F /Y
 
 :: x64
-xcopy "..\x64\Release\Jacobi.*" "lib\netcore\" /F /Y
-xcopy "..\Jacobi.Vst.Core\bin\x6\Release\netcoreapp3.1\Jacobi.*" "lib\netcore\" /F /Y
-xcopy "..\Jacobi.Vst.Framework\bin\x64\Release\netcoreapp3.1\Jacobi.*" "lib\netcore\" /F /Y
+xcopy "..\x64\Release\Jacobi.*" "runtimes\win10-x64\lib\netcoreapp3.1\" /F /Y
+xcopy "..\Jacobi.Vst.Core\bin\x6\Release\netcoreapp3.1\Jacobi.*" "runtimes\win10-x64\lib\netcoreapp3.1\" /F /Y
+xcopy "..\Jacobi.Vst.Framework\bin\x64\Release\netcoreapp3.1\Jacobi.*" "runtimes\win10-x64\lib\netcoreapp3.1\" /F /Y
 xcopy "..\x64\Release\ijwhost.dll" "runtimes\win10-x64\native\" /F /Y
 
 nuget pack VST.NET.nuspec
