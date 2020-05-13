4a151e566fab33b80b00d0ec02c21bcb1c2f51c6
Marc Jacobi <marc.jacobi@macaw.nl>
2020-05-13 20:04:26 +0200

    start cli

---
+++ Source.Core/Code/Jacobi.Vst.CLI/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace Jacobi.Vst.CLI
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            var deployPath = ".\\deploy";
+            EnsureDirectoryExists(deployPath);
+
+            var cmd = new PublishCommand
+            {
+                NuGetPath = GetNuGetLocation(),
+                DeployPath = deployPath
+            };
+
+            foreach (var fileName in Directory.EnumerateFiles(".", "*.deps.json"))
+            {
+                Console.WriteLine($"Publishing {fileName}.");
+                cmd.Publish(fileName);
+            }
+        }
+
+        private static string GetNuGetLocation()
+        {
+            var userPath = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            return Path.Combine(userPath, ".nuget", "packages");
+        }
+
+        private static void EnsureDirectoryExists(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
