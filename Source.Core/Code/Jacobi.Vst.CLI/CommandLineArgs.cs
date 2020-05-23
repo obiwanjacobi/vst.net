d128ccb5f47c9fa3f34435f8f8063ef2299e9d45
Marc Jacobi <marc.jacobi@macaw.nl>
2020-05-23 11:37:29 +0200

    publish plugin

---
+++ Source.Core/Code/Jacobi.Vst.CLI/CommandLineArgs.cs
@@ -0,0 +1,111 @@
+﻿using System;
+
+namespace Jacobi.Vst.CLI
+{
+    internal sealed class CommandLineArgs
+    {
+        private static readonly CommandInfo[] Commands = new[]
+        {
+            new CommandInfo { Name = "publish", Description="Gathers all dependencies based on a root .deps.json file.",
+                Arguments = new[] {
+                    new ArgumentInfo { Name = "-o", Description="The directory that will receive all the files." },
+                    new ArgumentInfo { Name = "-d", Description="The .deps.json file to publish." }
+                }
+            }
+        };
+
+        public CommandLineArgs(string[] args)
+        {
+            if (args != null && args.Length > 0)
+            {
+                Parse(args);
+            }
+        }
+
+        public ICommand Command { get; private set; }
+
+        private void Parse(string[] args)
+        {
+            var tokens = new Tokens(args);
+
+            while (tokens.MoveNext())
+            {
+                if (tokens.IsCommand)
+                {
+                    Command = ParseCommand(tokens);
+                }
+                else
+                {
+                    Ignore(tokens.Current);
+                }
+            }
+        }
+
+        private void Ignore(string ignore)
+        {
+            Console.WriteLine($"Ignoring command line arg: {ignore}");
+        }
+
+        private ICommand ParseCommand(Tokens tokens)
+        {
+            // TODO:
+            return null;
+        }
+
+        private sealed class Tokens
+        {
+            private readonly string[] _tokens;
+            private int _index;
+
+            public Tokens(string[] args)
+            {
+                _tokens = args;
+                _index = -1;
+            }
+
+            public bool IsCommand
+            {
+                get { return !Current.StartsWith('-'); }
+            }
+
+            public string Current
+            {
+                get
+                {
+                    if (!IsIndexInRange()) { throw new InvalidOperationException(); }
+
+                    return _tokens[_index];
+                }
+            }
+
+            public bool MoveNext()
+            {
+                if (IsIndexInRange())
+                {
+                    _index++;
+                    return true;
+                }
+                return false;
+            }
+
+            private bool IsIndexInRange()
+            {
+                return _index >= 0 && _index < _tokens.Length;
+            }
+        }
+
+
+        private class CommandInfo
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public ArgumentInfo[] Arguments { get; set; }
+        }
+
+        private class ArgumentInfo
+        {
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+    }
+}
--- Source.Core/Code/Jacobi.Vst.CLI/PublishCommand.cs
+++ Source.Core/Code/Jacobi.Vst.CLI/PublishCommand.cs
@@ -1,30 +1,157 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Linq;
 using System.Text.Json;
 
 namespace Jacobi.Vst.CLI
 {
-    internal class PublishCommand
+    internal class PublishCommand : ICommand
     {
+        private const string CommandInstructions = "vstnet publish <target> -o:<path>";
+
+        // Interop mixed C++/CLR project does not publish dependencies.
+        // Manually maintain them here.
+        private static readonly string[] InteropDependencies = new[]
+        {
+            @"Microsoft.Extensions.Configuration\3.1.2\lib\netcoreapp3.1\Microsoft.Extensions.Configuration.dll",
+            @"Microsoft.Extensions.Configuration.Abstractions\3.1.2\lib\netcoreapp3.1\Microsoft.Extensions.Configuration.Abstractions.dll",
+            @"Microsoft.Extensions.Configuration.FileExtensions\3.1.1\lib\netcoreapp3.1\Microsoft.Extensions.Configuration.FileExtensions.dll",
+            @"Microsoft.Extensions.Configuration.Json\2.1.0\lib\netstandard2.0\Microsoft.Extensions.Configuration.Json.dll",
+            @"Microsoft.Extensions.FileProviders.Physical\3.1.1\lib\netcoreapp3.1\Microsoft.Extensions.FileProviders.Physical.dll",
+            @"Microsoft.Extensions.FileProviders.Abstractions\3.1.1\lib\netcoreapp3.1\Microsoft.Extensions.FileProviders.Abstractions.dll",
+        };
+
+        public void Execute()
+        {
+            var depsFile = GetDepsFile();
+            if (depsFile == null)
+            {
+                ConsoleOutput.Warning($"Unable to find the .deps.json file based on {FilePath}. Cannot copy dependencies.");
+            }
+            else
+            {
+                CopyDependencies(depsFile);
+            }
+
+            var plugin = GetPluginFile();
+            var host = GetHostFile();
+
+            if (plugin != null)
+            {
+                PublishPlugin(plugin);
+                return;
+            }
+
+            if (host != null)
+            {
+                PublishHost(host);
+                return;
+            }
+
+            if (depsFile != null)
+            {
+                ConsoleOutput.Warning($"Unable to find the code file (.dll/.exe) based on {FilePath}. Cannot perform code publication.");
+            }
+            else
+            {
+                ConsoleOutput.Help(CommandInstructions);
+            }
+        }
+
         public string NuGetPath { get; set; }
         public string DeployPath { get; set; }
+        public string FilePath { get; set; }
 
-        public void Publish(string fileName)
+        private void CopyDependencies(string depsFile)
         {
-            using var stream = File.OpenRead(fileName);
+            ConsoleOutput.Progress($"Copy Dependencies to {DeployPath}");
+            using var stream = File.OpenRead(depsFile);
             var json = Parse(stream);
             var paths = json.Targets.First().Value.GetFilePaths();
 
-            foreach (var path in paths)
+            foreach (var path in paths.Concat(InteropDependencies).Distinct())
             {
                 var filePath = Path.Combine(NuGetPath, path);
                 if (File.Exists(filePath))
                 {
-                    Console.WriteLine(filePath);
+                    ConsoleOutput.Progress(filePath);
                     File.Copy(filePath, Path.Combine(DeployPath, Path.GetFileName(filePath)), overwrite: true);
                 }
+                else
+                {
+                    ConsoleOutput.Warning($"Could not find: {filePath}");
+                }
             }
+        }
+
+        private void PublishPlugin(string pluginPath)
+        {
+            var name = Path.GetFileNameWithoutExtension(pluginPath);
+            //var path = Path.GetDirectoryName(pluginPath);
+
+            var managed = Path.Combine(DeployPath, $"{name}.net.vstdll");
+            // copy over and rename the managed plugin dll.
+            File.Copy(pluginPath, managed, overwrite: true);
+            ConsoleOutput.Progress($"Renaming managed plugin: {managed}");
+
+            var interop = Path.Combine(DeployPath, "Jacobi.Vst.Interop.dll");
+            var entry = Path.Combine(DeployPath, $"{name}.dll");
+            // rename Jacobi.Vst.Interop to plugin name
+            File.Copy(interop, entry, overwrite: true);
+            File.Delete(interop);
+            ConsoleOutput.Progress($"Creating unmanged plugin: {entry}");
+
+            // copy in all other plugin related file (Debug?)
+            //foreach (var sourceFile in Directory.EnumerateFiles(path, $"{name}.*"))
+            //{
+            //    var targetName = Path.GetFileName(sourceFile);
+            //    var targetFile = Path.Combine(DeployPath, targetName);
+            //    File.Copy(sourceFile, targetFile, overwrite: true);
+            //    ConsoleOutput.Progress($"Copy target file: {sourceFile}");
+            //}
+
+            var runtimeConfig = Path.Combine(DeployPath, $"{name}.runtimeconfig.json");
+            if (!File.Exists(runtimeConfig))
+            {
+                var assembly = typeof(PublishCommand).Assembly;
+                using var stream = assembly.GetManifestResourceStream("Jacobi.Vst.CLI.runtimeconfig.json");
+                var reader = new StreamReader(stream);
+                File.WriteAllText(runtimeConfig, reader.ReadToEnd());
+
+                ConsoleOutput.Progress($"Creating {runtimeConfig}");
+            }
+        }
+
+        private void PublishHost(string hostPath)
+        {
+            // TODO: make sure ijwhost.dll is present
+        }
+
+        private string GetDepsFile()
+        {
+            return GetFileByExtension(FilePath, ".deps.json");
+        }
+
+        private string GetPluginFile()
+        {
+            return GetFileByExtension(FilePath, ".dll");
+        }
+
+        private string GetHostFile()
+        {
+            return GetFileByExtension(FilePath, ".exe");
+        }
+
+        private static string GetFileByExtension(string baseFile, string extension)
+        {
+            if (baseFile.EndsWith(extension)) return baseFile;
+
+            var name = Path.GetFileNameWithoutExtension(baseFile);
+            var path = Path.GetDirectoryName(baseFile);
+            var file = Path.Combine(path, $"{name}{extension}");
+
+            if (File.Exists(file)) { return file; }
+
+            return null;
         }
 
         private static DepsJson Parse(Stream stream)
