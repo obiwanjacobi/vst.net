dfe60509efb34ce336edfbdfd2e7b42b7e65f005
Marc Jacobi <marc.jacobi@macaw.nl>
2020-05-25 20:17:16 +0200

    nuget troubles

--- Source.Core/Code/Jacobi.Vst.Deployment/pack.cmd
+++ Source.Core/Code/Jacobi.Vst.Deployment/pack.cmd
@@ -1,13 +1,1 @@
-:: x86
-xcopy "..\Release\Jacobi.Vst.Interop.*" "pack\runtimes\win10-x86\lib\netcoreapp3.1\" /F /Y
-xcopy "..\Jacobi.Vst.Core\bin\x86\Release\netcoreapp3.1\Jacobi.Vst.Core.*" "pack\runtimes\win10-x86\lib\netcoreapp3.1\" /F /Y
-xcopy "..\Jacobi.Vst.Framework\bin\x86\Release\netcoreapp3.1\Jacobi.Vst.Framework.*" "pack\runtimes\win10-x86\lib\netcoreapp3.1\" /F /Y
-xcopy "..\Release\ijwhost.dll" "pack\runtimes\win10-x86\native\" /F /Y
-
-:: x64
-xcopy "..\x64\Release\Jacobi.Vst.Interop.*" "pack\runtimes\win10-x64\lib\netcoreapp3.1\" /F /Y
-xcopy "..\Jacobi.Vst.Core\bin\x64\Release\netcoreapp3.1\Jacobi.Vst.Core.*" "pack\runtimes\win10-x64\lib\netcoreapp3.1\" /F /Y
-xcopy "..\Jacobi.Vst.Framework\bin\x64\Release\netcoreapp3.1\Jacobi.Vst.Framework.*" "pack\runtimes\win10-x64\lib\netcoreapp3.1\" /F /Y
-xcopy "..\x64\Release\ijwhost.dll" "pack\runtimes\win10-x64\native\" /F /Y
-
 nuget pack .\pack\VST.NET.nuspec
--- Source.Core/Code/Jacobi.Vst.Deployment/push-test.cmd
+++ Source.Core/Code/Jacobi.Vst.Deployment/push-test.cmd
@@ -1,1 +1,1 @@
-nuget push Jacobi.Vst.2.0.0-alpha.nupkg -src LocalPackageTest
+nuget push VST.NET2.2.0.0-alpha.nupkg -src LocalPackageTest
