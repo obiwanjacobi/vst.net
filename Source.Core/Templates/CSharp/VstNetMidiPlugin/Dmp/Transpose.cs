2be9d4caaedc3915559ec389585957e611139b88
Marc Jacobi <marc.jacobi@macaw.nl>
2020-06-02 17:54:24 +0200

    add VS project templates

---
+++ Source.Core/Templates/CSharp/VstNetMidiPlugin/Dmp/Transpose.cs
@@ -0,0 +1,94 @@
+﻿using Jacobi.Vst.Core;
+using Jacobi.Vst.Plugin.Framework;
+
+namespace VstNetMidiPlugin.Dmp
+{
+    /// <summary>
+    /// Change the Note Number of MIDI notes.
+    /// </summary>
+    internal sealed class Transpose
+    {
+        private static readonly string ParameterCategoryName = "Trasnpose";
+
+        private readonly Plugin _plugin;
+
+        public Transpose(Plugin plugin)
+        {
+            _plugin = plugin;
+
+            InitializeParameters();
+
+            _plugin.Opened += new System.EventHandler(Plugin_Opened);
+        }
+
+        private void Plugin_Opened(object sender, System.EventArgs e)
+        {
+            TransposeMgr.HostAutomation = _plugin.Host.GetInstance<IVstHostAutomation>();
+
+            _plugin.Opened -= new System.EventHandler(Plugin_Opened);
+        }
+
+        public VstParameterManager TransposeMgr { get; private set; }
+
+        private void InitializeParameters()
+        {
+            // all parameter definitions are added to a central list.
+            VstParameterInfoCollection parameterInfos = _plugin.PluginPrograms.ParameterInfos;
+
+            // retrieve the category for all delay parameters.
+            VstParameterCategory paramCategory =
+                _plugin.PluginPrograms.GetParameterCategory(ParameterCategoryName);
+
+            // delay time parameter
+            VstParameterInfo paramInfo = new VstParameterInfo
+            {
+                Category = paramCategory,
+                CanBeAutomated = true,
+                Name = "Transp.",
+                Label = "Halfs",
+                ShortLabel = "#",
+                MinInteger = -100,
+                MaxInteger = 100,
+                LargeStepFloat = 5.0f,
+                SmallStepFloat = 1.0f,
+                StepFloat = 2.0f,
+                DefaultValue = 0.0f
+            };
+            TransposeMgr = new VstParameterManager(paramInfo);
+            VstParameterNormalizationInfo.AttachTo(paramInfo);
+
+            parameterInfos.Add(paramInfo);
+        }
+
+        public VstMidiEvent ProcessEvent(VstMidiEvent inEvent)
+        {
+            if (!MidiHelper.IsNoteOff(inEvent.Data) && !MidiHelper.IsNoteOn(inEvent.Data))
+            {
+                return inEvent;
+            }
+
+            byte[] outData = new byte[4];
+            inEvent.Data.CopyTo(outData, 0);
+
+            outData[1] += (byte)TransposeMgr.CurrentValue;
+
+            if (outData[1] > 127)
+            {
+                outData[1] = 127;
+            }
+
+            if (outData[1] < 0)
+            {
+                outData[1] = 0;
+            }
+
+            // MidiEvents are immutable, 
+            // so we create a new object for the new data.
+            VstMidiEvent outEvent = new VstMidiEvent(
+                inEvent.DeltaFrames, inEvent.NoteLength, inEvent.NoteOffset,
+                outData, inEvent.Detune, inEvent.NoteOffVelocity);
+
+            return outEvent;
+        }
+    }
+}
---
+++ Source.Core/Templates/CSharp/VstNetMidiPlugin/UI/PluginEditorView.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace VstNetMidiPlugin.UI
+{
+    partial class PluginEditorView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar1.Location = new System.Drawing.Point(75, 3);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(145, 56);
+            this.trackBar1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 3;
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 32.14286F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 67.85714F));
+            tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 63F));
+            tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+            tableLayoutPanel1.Controls.Add(this.trackBar1, 1, 0);
+            tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+            tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
+            tableLayoutPanel1.Controls.Add(this.label4, 2, 1);
+            tableLayoutPanel1.Controls.Add(this.trackBar2, 1, 1);
+            tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            tableLayoutPanel1.Size = new System.Drawing.Size(287, 122);
+            tableLayoutPanel1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(226, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(226, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.trackBar2.Location = new System.Drawing.Point(75, 65);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(145, 56);
+            this.trackBar2.TabIndex = 4;
+            // 
+            // PluginEditorView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "PluginEditorView";
+            this.Size = new System.Drawing.Size(287, 122);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar2;
+
+    }
+}
