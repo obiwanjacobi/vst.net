dff68eca7d8fee958fea85c76a5021bbaddc5f1d
Marc Jacobi <marc.jacobi@macaw.nl>
2021-09-17 10:52:22 +0200

    cannot return plugin factory interface

--- Source3/Code/Jacobi.Vst3.Interop/dllmain.cpp
+++ Source3/Code/Jacobi.Vst3.Interop/dllmain.cpp
@@ -1,17 +1,12 @@
-#include "pch.h"
-#include <delayimp.h>
-
-//HMODULE g_hModule;
-
-//char buffer[MAX_PATH + 1];
-// Get full path to ijwhost.dll based on current module path
-//auto res = GetModuleFileNameA(g_hModule, buffer, MAX_PATH + 1);
-
 #pragma unmanaged
+
+#include "pch.h"
+
+HMODULE g_hModule;
 
 extern "C"
 {
-    /*BOOL APIENTRY DllMain(HMODULE hModule,
+    BOOL APIENTRY DllMain(HMODULE hModule,
         DWORD  ul_reason_for_call,
         LPVOID lpReserved)
     {
@@ -25,23 +20,7 @@ extern "C"
             break;
         }
         return TRUE;
-    }*/
-
-    FARPROC WINAPI delayLoadFailureHook(unsigned dliNotify, PDelayLoadInfo pdli)
-    {
-        if (dliNotify != dliFailLoadLib ||
-            _strcmpi(pdli->szDll, "ijwhost.dll") != 0) {
-            return NULL;
-        }
-
-
-        ::OutputDebugStringA("Loading Ijwhost.dll");
-
-        const char* path = "C:\\Users\\marc\\Documents\\MyProjects\\public\\Jacobi\\Public\\GitHub\\vst.net\\Source3\\Code\\x64\\Debug\\Ijwhost.dll";
-        return (FARPROC)::LoadLibraryA(path);
     }
-
-    const PfnDliHook __pfnDliFailureHook2 = delayLoadFailureHook;
 }
 
 #pragma managed
---
+++ Source3/Code/Jacobi.Vst3.TestPlugin/PublicExport.cs
@@ -0,0 +1,26 @@
+﻿using Jacobi.Vst3.Core;
+using System.Runtime.InteropServices;
+
+namespace Jacobi.Vst3.TestPlugin
+{
+    public static class PublicExport
+    {
+        [UnmanagedCallersOnly]
+        public static bool InitDll()
+        {
+            return true;
+        }
+
+        [UnmanagedCallersOnly]
+        public static void ExitDll()
+        {
+            // no-op
+        }
+
+        [UnmanagedCallersOnly]
+        public static IPluginFactory GetPluginFactory()
+        {
+            return new PluginFactory();
+        }
+    }
+}
