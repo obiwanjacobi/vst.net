34b07e95844f64bfb67b939079b7c3678c06ae18
marc <obiwanjacobi@hotmail.com>
2024-03-09 11:21:37 +0100

    last tweaks

--- Source/Code/Jacobi.Vst.Deployment/push-nuget.cmd
+++ Source/Code/Jacobi.Vst.Deployment/push-nuget.cmd
@@ -1,2 +1,2 @@
-nuget push VST.NET2-Host.2.1.0.nupkg -src https://api.nuget.org/v3/index.json
-nuget push VST.NET2-Plugin.2.1.0.nupkg -src https://api.nuget.org/v3/index.json
+nuget push VST.NET2-Host.2.1.1.nupkg -src https://api.nuget.org/v3/index.json
+nuget push VST.NET2-Plugin.2.1.1.nupkg -src https://api.nuget.org/v3/index.json
